5dbcc504b793fd381ce31f7627a1607536d5b4aa
RomanOrtiz75 <romanabrahanortiz@gmail.com>
2023-01-24 13:41:01 -0600

    agregamos el login y las ext

---
+++ components/user/login.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-card class="cardLogin">
+        <v-card-title class="title">Wuelcon to el barsa</v-card-title>
+        <v-card-text>
+            <v-row justify="center" align="center">
+                <v-col cols="4" justify="center" align-self="center">
+                    <img src="../../assets/images/goku.png" class="imgLogin">
+                </v-col>
+                <v-col cols="8" justify="center" align-self="center">
+                    <v-form ref="formLogin">
+                <v-text-field label="Ingresa tu correo electronico" 
+                              placeholder="ejemplo: hola@outlook.com"
+                              v-model="correoElectronico"
+                              :rules="validarCorreo"
+                />
+                <v-text-field label="Password" 
+                              placeholder="Password"
+                              v-model="password"
+                              :rules="validarPassword"
+                />
+            </v-form>
+                </v-col>
+            </v-row>
+            
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+            class="btnLogin"
+            rounded
+            block
+            @click="loginBackend"
+            >
+                Iniciar
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            correoElectronico: '',
+            validarCorreo: [
+            v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+            ],
+            password:'',
+            validarPassword: [
+                value => value.length >= 6 || 'Min 6 characters'
+            ]
+        }
+    },
+    methods: {
+        loginBackend () {
+            const valid = this.$refs.formLogin.validate()
+            if (valid) {
+                alert('presione el btn')
+            }else {
+                alert('no cumpliste las reglas')
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .cardLogin{
+        background-color: #4894d3;
+        border-radius: 10px;
+        width: 500px;
+        height: 300px;
+    }
+
+    .imgLogin {
+        width: 100%;
+        height: 100%;
+    }
+
+    .btnLogin{
+        background-color: #cc3737!important;
+        color: rgb(255, 255, 255);
+    }
+
+    .title {
+        font-size: 30px;
+        justify-content: center;
+        color: #cc3737;
+        font-weight: 700;
+    }
+</style>
---
+++ layouts/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-row class="renglon" >
+        <nuxt />
+    </v-row>
+</template>
+
+<style scoped>
+    .renglon{
+        width: 100vw;
+        height: 100vh;
+        flex-direction: row;
+        align-content: center;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
